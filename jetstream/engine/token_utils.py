a05c0f37f56dc0dff94d3d568dbacecc5efbb3bc
FBS <aliu2000@outlook.com>
2024-10-19 15:17:24 +0800

    test

--- jetstream/engine/token_utils.py
+++ jetstream/engine/token_utils.py
@@ -227,7 +227,7 @@ def process_result_tokens(
     )
   return_samples = []
   for idx in range(samples):
-    text_so_far = []
+    #text_so_far = []
     tok_id_so_far = []
     semantics_tok_id_so_far = []
     if not complete[idx].item():
@@ -253,7 +253,7 @@ def process_result_tokens(
           tok_id_so_far.append(tok_id)
           semantics_tok_id_so_far.append(slot_semantics[idx, spec_idx])
     return_samples.append(
-        ReturnSample(text=text_so_far, token_ids=semantics_tok_id_so_far)
+        ReturnSample(token_ids=semantics_tok_id_so_far)
     )
     if debug:
       logging.info("Return samples %s", str(return_samples))
